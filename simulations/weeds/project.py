a450e63bc850e058641e56a1809a861e2288f2ee
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-08-19 20:16:54 +1000

    setup weeds simulation

---
+++ simulations/weeds/project.py
@@ -0,0 +1,35 @@
+"""Common filenames for Merimbula simulation
+"""
+import anuga
+
+from datetime import datetime
+
+now = datetime.now() # current date and time
+
+date_time = now.strftime("%Y%m%d_%H%M%S")
+
+print("date and time:",date_time)
+
+model_dir = '../../model_data_sept_2003'
+mesh_dir  = '../../meshes'
+weed_dir  = '../../weed_zones'
+
+original_boundary_filename = anuga.join(model_dir,'Eden_tide_Sept03.dat')
+original_wind_filename     = anuga.join(model_dir,'merimbula_wind_sept_2003.dat')
+boundary_filename          = anuga.join(model_dir,'Eden_tide_Sept03.tms')
+gauge_filename             = anuga.join(model_dir,'gauge_locations.csv')
+
+bathymetry_filename        = anuga.join(mesh_dir,'merimbula_bathymetry.xya')
+mesh_filename              = anuga.join(mesh_dir,'merimbula_10785.tsh')
+
+depth_canal = 4
+simulation_name            = f'domain_10785_canal_{depth_canal}_{date_time}'
+
+sec = 1.0
+min = 60*sec
+hr  = 60*min
+day = 24*hr
+
+yieldstep  = 5*min
+outputstep = 15*min
+finaltime  = 30*min
