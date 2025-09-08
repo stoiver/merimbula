00631c2fec027d43bea4cb8204439302410585f6
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-09-08 20:20:16 +1000

    add Eden tide data with timestamp

---
+++ model_data_sept_2003/Eden_dat_to_csv_with_timestamp.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+file_name = 'Eden_tide_Sept03.dat'
+
+df = pd.read_csv(file_name, sep=r'[,\s]+', engine='python', names=['Date', 'Time', 'Stage', 'Xmom', 'Ymom'])
+df['DateTime'] = pd.to_datetime(df['Date'].astype(str) + ' ' + df['Time'].astype(str), dayfirst=True, format='%d/%m/%y %H:%M:%S')
+df['timestamp'] = df['DateTime'].values.astype(np.int64) // 10**9
+
+df.to_csv('Eden_tide_Sept03.csv')
