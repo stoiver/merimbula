b1e56d4a6553be16109696f81956a8e3eabf4283
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-12-02 20:01:12 +1100

    add a new template directory

---
+++ simulations/template/project.py
@@ -0,0 +1,58 @@
+"""Common filenames for Merimbula simulation
+"""
+
+#mesh_size = 10785
+#mesh_size = '17156' # currently problem with these meshes 
+mesh_size = 43200
+
+model_date = 'sept_2003'
+#model_date = 'sept_2004'
+
+weeds = True
+canal = True
+depth_canal = 4
+
+simulation_dir = 'combined'
+
+sec = 1.0
+min = 60*sec
+hr  = 60*min
+day = 24*hr
+
+yieldstep  = 10*sec
+outputstep = 5*min
+duration   = 19*min #5*day
+
+
+
+
+#------------------------------------
+# No need to edit below this line
+#------------------------------------
+import anuga
+
+georef = anuga.Geo_reference(zone = 55,  hemisphere = 'southern')
+
+from datetime import datetime
+now = datetime.now() # current date and time
+date_time = now.strftime("%Y%m%d_%H%M%S")
+print("date and time:",date_time)
+
+
+model_dir = f'../../model_data_{model_date}'
+mesh_dir  = '../../meshes'
+weed_dir  = '../../weed_zones'
+
+
+boundary_filename          = anuga.join(model_dir,'boundary.csv')
+gauge_filename             = anuga.join(model_dir,'gauge_locations.csv')
+
+bathymetry_filename        = anuga.join(mesh_dir,'merimbula_bathymetry.xya')
+mesh_filename              = anuga.join(mesh_dir,f'merimbula_{mesh_size}.tsh')
+
+if weeds:
+    simulation_name            = f'{simulation_dir}_{model_date}_{mesh_size}_weeds_{date_time}'
+else:
+    simulation_name            = f'{simulation_dir}_{model_date}_{mesh_size}_no_weeds_{date_time}'
+
+
---
+++ simulations/template/run_merimbula.py
@@ -0,0 +1,246 @@
+"""
+Main meribula script using new interface
+"""
+
+#-------------------------------
+# Module imports
+#-------------------------------
+import sys, os
+
+import pandas as pd
+from scipy.interpolate import interp1d
+
+import anuga
+import project
+from project import yieldstep, outputstep, duration
+from project import weeds, canal, depth_canal
+
+from anuga import log
+log.log_filename = 'run_merimbula.log'
+
+weed_dir = project.weed_dir
+
+#-------------------------------
+# Domain
+#-------------------------------
+
+if anuga.myid == 0:
+    print ('Creating domain from', project.mesh_filename)
+
+    domain = anuga.pmesh_to_domain_instance(project.mesh_filename, anuga.Domain, use_cache=True)
+
+    domain.set_name(project.simulation_name)
+    domain.check_integrity()
+    domain.set_georeference(project.georef)
+
+    print ('Number of triangles = ', len(domain))
+    print ('The extent is ', domain.get_extent())
+
+
+    #-------------------------------
+    # Initial Conditions
+    #-------------------------------
+
+    #elevation_offset=0.1
+
+    print ('Initial values')
+    bathymetry_filename =  project.bathymetry_filename[:-4] + '.xya'
+
+    print (bathymetry_filename)
+
+    domain.set_quantity('elevation',
+                        filename = bathymetry_filename,
+                        alpha = 0.5,
+                        verbose = True,
+                        use_cache = True)
+
+    #domain.set_quantity('elevation',expression='elevation +%f' %elevation_offset)
+    #domain.set_quantity('friction', 0.01)
+    domain.set_quantity('stage', 0.0)
+
+else:
+    domain = None
+
+domain = anuga.distribute(domain)
+
+#-------------------------------
+# Setup domain runtime parameters
+#-------------------------------
+
+domain.store = True    #Store for visualisation purposes
+domain.smooth = False
+domain.set_low_froude(1)
+domain.set_flow_algorithm('DE1')
+try:
+    domain.set_multiprocessor_mode(1)
+except:
+    pass
+
+
+print (f'Stats for domain on rank {anuga.myid}')
+print (domain.statistics())
+
+
+if weeds:
+    from weed_zones import set_friction_from_weed_zones
+    set_friction_from_weed_zones(domain, weed_dir)
+
+# domain.set_plotter()
+# import matplotlib.pyplot as plt
+
+# import matplotlib.cm as cm
+# import numpy as np
+
+# domain.tripcolor(
+#               facecolors=np.sqrt(domain.stage - domain.elev),
+#               cmap=cm.viridis,  # A visually appealing colormap
+#               vmin=0.0,
+#               vmax=2.0  # Adjusted to cover the range of friction values
+#               )
+# plt.colorbar()
+# plt.show()
+
+#--------------------------------
+# dredge out the canal
+#--------------------------------
+if canal:
+    canal_polygon = [[759222.474012,5912903.796898],
+            [759191.946009,5912861.297128],
+            [759224.269777,5912866.684423],
+            [759242.100000,5912879.000000],
+            [759252.700000,5912892.000000],
+            [759256.593546,5912915.170076],
+            [759242.826015,5912939.113609],
+            [759228.000000,5912954.000000],
+            [759209.600000,5912931.000000],
+            [759193.800000,5912906.000000],
+            [759170.000000,5912890.000000]]
+
+    domain.set_quantity('elevation',numeric = -depth_canal,
+                   polygon = canal_polygon,
+                   smooth = True,
+                   verbose = True,
+                   location = 'centroids',
+                   use_cache = True)
+
+
+
+
+
+#-------------------------------
+# Boundary conditions
+#-------------------------------
+if anuga.myid == 0: 
+    print ('Boundaries')
+
+#----------------------------------------
+#   Tidal cycle recorded at Eden as open
+#----------------------------------------
+if anuga.myid == 0:
+    print ('Open sea boundary condition from ',project.boundary_filename)
+
+
+tide_df = pd.read_csv(project.boundary_filename)
+tide_function = interp1d(tide_df['timestamp'], tide_df['Stage'], kind='linear', fill_value=0.0, bounds_error=False)
+
+
+#--------------------------------------
+#  Boundaries, open boundary with tide
+#  elsewhere reflective
+#--------------------------------------
+Br = anuga.Reflective_boundary(domain)
+Bf = anuga.Flather_external_stage_zero_velocity_boundary(domain, function = tide_function)
+
+domain.set_boundary({'exterior': Br, 'open': Bf})
+
+#-------------------------------------------------------
+# Set the start time of the simulation to the first time in the tide data
+#-------------------------------------------------------
+domain.set_starttime(tide_df['timestamp'].iloc[0])
+domain.set_timezone('Australia/Sydney')
+
+# 26th October 2003 01:45:00, just before change from 
+# AEST (UTC+10) to AEDT (UTC+11  at 2am)
+# domain.set_starttime(tide_df['timestamp'].iloc[3271])
+
+#-------------------------------
+# Find a triangle next to mid point of tide boundary
+#-------------------------------
+
+import numpy as np
+p0 = np.array([761052.7, 5912151.0])
+p1 = np.array([759608.8, 5912326.0])
+
+x0 = 0.5*(p0[0]+p1[0])
+y0 = 0.5*(p0[1]+p1[1])
+
+try:
+    tid = domain.get_triangle_containing_point([ x0-9, y0+6])
+except:
+    tid = None
+
+print (f'Triangle id next to middle of tide boundary: {tid}')
+#print (domain.centroid_coordinates[9433])
+
+anuga.barrier()
+
+#-------------------------------
+# Evolve
+#-------------------------------
+import time
+t0 = time.time()
+
+if anuga.myid == 0:
+    import os
+    num_threads = os.environ.get('OMP_NUM_THREADS')
+    if num_threads is not None:
+        num_threads = int(num_threads)
+    else:
+        num_theads = 1     
+    print (' ')
+    print ('#',60*'=')
+    print ('#','Evolving domain')
+    print ('#',60*'=')
+    print ('#','Number of MPI processes = ', anuga.numprocs)
+    print ('#','Number of OPENMP threads = ', num_threads)
+    print ('#','Multiprocessor Mode = ', domain.multiprocessor_mode)
+    print ('#','Yield step = ', yieldstep)
+    print ('#','Output step = ', outputstep)
+    print ('#','Duration  = ', duration)
+    print ('#',60*'=')
+    print (' ')
+#===========================================================================
+# Main Evolve Loop
+#===========================================================================
+
+for t in domain.evolve(yieldstep = yieldstep, outputstep = outputstep, duration = duration):
+        
+    # This only happens on processor that owns the triangle.
+    if tid is not None:
+        print (domain.timestepping_statistics(datetime = True))
+        print (f'    Tide {tide_function(t):.3f}, Mid Boundary Stage {domain.get_quantity("stage").centroid_values[tid]:.3f} ')
+        sys.stdout.flush()
+
+anuga.barrier()
+
+if anuga.myid == 0:
+    print (f'That took {(time.time()-t0):.2f} seconds on {anuga.numprocs} MPI processes and {num_threads} OPENMP threads')
+
+
+
+anuga.barrier()
+
+
+if anuga.numprocs > 1:
+
+    if anuga.myid == 0:
+        print (' ')
+        print ('#',60*'=')
+        print ('#','Merging partitioned sww files')
+        print ('#',60*'=')
+        print (' ')
+        
+    domain.sww_merge(delete_old=True)
+
+    anuga.finalize()
+
